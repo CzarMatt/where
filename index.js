8cfe7786e56ac902b2cd84ce9421097ec0791138
Matt Mossman <czarmatt@gmail.com>
2019-03-29 13:01:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,8 @@ const Http = new XMLHttpRequest();
 const url = "https://slack.com/api/channels.info?token="+(botId)+"&channel=C0EGJMMM5";
 
 Http.onreadystatechange = (e) => {
-    var r = JSON.parse(Http.responseText).channel.topic.value;
+    var json = JSON.parse(Http.responseText);
+    var r = json.channel.topic.value;
     console.info("Received response: " + r);
     try {
         if (Http.status == 200) {
